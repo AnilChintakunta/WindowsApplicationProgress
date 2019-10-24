d72629465ccc2466d3dd42ace9d0d09d4b5ce545
Anil.Chintakunta <anil.chintakunta@infor.com>
2019-10-24 15:31:05 +0530

    Code CHanges

--- WindowsFormsApp1/Form2.cs
+++ WindowsFormsApp1/Form2.cs
@@ -13,57 +13,53 @@ namespace WindowsFormsApp1
 {
     public partial class Form2 : Form
     {
+        public static string ServiceResult = "InProgress";
+        string statusText = "";
+        string[] statusValues = new string[] { "Running 1", "Running 2", "Running 3", "Running 4", "Running 5", "Running 6", "Running 7", "Running 8", "Running 9", "Running 10", "Running 11" };
         public Form2()
         {
             InitializeComponent();
         }
-
         private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
         {
             backgroundWorker1.WorkerReportsProgress = true;
             backgroundWorker1.RunWorkerAsync();
         }
-
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
         {
-            //API Call 1 Report progress.  
-            backgroundWorker1.ReportProgress(1);
-
-
-
-            //API Call 2 Report progress.  
-            Thread.Sleep(2000);
-            backgroundWorker1.ReportProgress(2);
-
-
-            //API Call 3 Report progress.  
-            Thread.Sleep(2000);
-            backgroundWorker1.ReportProgress(3);
-
-
-
-            //API Call 4 Report progress.  
-            Thread.Sleep(2000);
-            backgroundWorker1.ReportProgress(4);
-
-            //API Call 5 Report progress.  
-            Thread.Sleep(2000);
-            backgroundWorker1.ReportProgress(5);
-
-
-            //API Call 6 Report progress.  
-            Thread.Sleep(2000);
-            backgroundWorker1.ReportProgress(6);
+            Task<bool> task = Method1();
+            foreach (string status in statusValues)
+            {
+                Thread.Sleep(1000);
+                statusText = status;
+                backgroundWorker1.ReportProgress(1);
+            }
 
         }
-
-        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
-        {           
-            label1.Text = "API Service CALL: " + e.ProgressPercentage.ToString();
-            if (e.ProgressPercentage == 6)
+        public static async Task<bool> Method1()
+        {
+            await Task.Run(() =>
             {
-                this.Close();
+                Thread.Sleep(10000);
+                ServiceResult = "Completed";
+            });
+            return true;
+        }
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            label1.Text = statusText;
+            if (ServiceResult == "Completed")
+            {
+                this.Close();           
             }
+        }
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
--- WindowsFormsApp1/Form2.designer.cs
+++ WindowsFormsApp1/Form2.designer.cs
@@ -30,6 +30,7 @@
         {
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // backgroundWorker1
@@ -40,17 +41,28 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(74, 149);
+            this.label1.Location = new System.Drawing.Point(157, 160);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(51, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(58, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(227, 49);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Call Service";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(426, 403);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form2";
@@ -65,5 +77,6 @@
         #endregion
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
